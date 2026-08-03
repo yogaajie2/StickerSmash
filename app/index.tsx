ab6abed064c5a59269386102a7efca0576e17bcf
Yoga Prasetya <yogaajie2@gmail.com>
2026-08-03 14:38:01 +0700

    refactor: update home screen styling

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,22 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Home screen</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    backgroundColor: "#25292e",
+    flex: 1,
+    justifyContent: "center",
+  },
+
+  text: {
+    color: "#fff",
+  },
+});
